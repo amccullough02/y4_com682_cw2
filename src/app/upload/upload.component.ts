4276ea99e4e4350383a10a0bfe2c13bf59c5e7b4
Alex McCullough <alexmccullough@outlook.com>
2024-12-01 19:56:30 +0000

    feat: editing of images

--- src/app/upload/upload.component.ts
+++ src/app/upload/upload.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { WebService } from '../web.service';
 import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
--- src/app/view/view.component.ts
+++ src/app/view/view.component.ts
@@ -1,20 +1,25 @@
 import { Component } from '@angular/core';
-import { ActivatedRoute, Router, RouterOutlet } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { WebService } from '../web.service';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'home-page',
   standalone: true,
+  imports: [ReactiveFormsModule, CommonModule],
   providers: [WebService],
   templateUrl: './view.component.html',
 })
 export class ViewComponent {
   media_data: any;
+  updateForm!: FormGroup;
 
   constructor(
     public webService: WebService,
     private route: ActivatedRoute,
-    private router: Router
+    private router: Router,
+    private formBuilder: FormBuilder
   ) {}
 
   ngOnInit() {
@@ -23,11 +28,30 @@ export class ViewComponent {
       .subscribe((response: any) => {
         this.media_data = response;
       });
+
+    this.updateForm = this.formBuilder.group({
+      category: [''],
+      description: [''],
+    });
   }
 
   decodeB64(data: string) {
     let decodedData = atob(data);
     return decodedData;
+  }
+
+  onUpdate() {
+    const formData = new FormData();
+    formData.append('category', this.updateForm.get('category')?.value);
+    formData.append('description', this.updateForm.get('description')?.value);
+    this.webService
+      .editMedia(formData, this.route.snapshot.paramMap.get('id'))
+      .subscribe(() => {
+        console.log('Media updated');
+        this.router.navigateByUrl('/', {skipLocationChange: true}).then(() => {
+          this.router.navigate([`/gallery/${this.route.snapshot.paramMap.get('id')}`]);
+        })
+      });
   }
 
   onDelete() {
