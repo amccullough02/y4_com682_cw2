c6e5777a7cfad9c421d95bee8db720674959b274
Alex McCullough <alexmccullough@outlook.com>
2024-12-03 14:23:44 +0000

    feat: chatbot integration

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,26 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import * as WebChat from 'botframework-webchat';
 
 @Component({
   selector: 'home-page',
   standalone: true,
   templateUrl: './home.component.html',
 })
-export class HomeComponent {}
+export class HomeComponent {
+  ngOnInit() {
+    const directLineSecret = process.env['DIRECT_LINE_SECRET']
+    const directLine = WebChat.createDirectLine({
+      secret: directLineSecret
+    });
+
+    WebChat.renderWebChat(
+      {
+        directLine,
+        userID: 'stellarsight-user',
+        username: 'Stellarsight Visitor',
+        locale: 'en-us',
+      },
+      document.getElementById('webchat')
+    );
+  }
+}
